6c6d57e1d66678708e9b997812792c22bde18b3c
Kushal Ghimire <ghimire.kushal@ymail.com>
2024-07-13 16:36:36 +0545

    CHORE[BACKEND]: refactored to run frontend and backend independently in local servers

--- backend/index.js
+++ backend/index.js
@@ -17,17 +17,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Serve tatic files from the React app
-const reactAppPath = path.join(__dirname, 'public');
-app.use(express.static(reactAppPath));
-
 app.use('/auth', authRoutes);
 app.use('/tasks', taskRoutes);
-
-// Fallback route to serve the index.html file for any other routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(reactAppPath, 'index.html'));
-});
 
 const PORT = process.env.PORT || 3000;
 
